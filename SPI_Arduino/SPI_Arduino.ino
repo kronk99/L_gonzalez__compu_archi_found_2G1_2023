c2806432c3cf71f6fef1147f3a7d2e8b87a096f7
mrr79 <marianarojas2197@gmail.com>
2024-05-09 21:21:46 -0600

    envio de handshake

--- SPI_Arduino/SPI_Arduino.ino
+++ SPI_Arduino/SPI_Arduino.ino
@@ -1,43 +1,43 @@
 #include <SPI.h>
-
 bool handshakeConfirmation;
-bool connection = true;
+bool connection=true;;
 
 void setup() {
     Serial.begin(9600);
     SPI.begin();
     SPI.setClockDivider(SPI_CLOCK_DIV2);
     Serial.println("ENVIANDO HANDSHAKE");
+    //digitalWrite(SS, LOW);
     handshake();
-    connection = true;
+    connection=true;
 }
 
 void loop() {
-    // Resto del código para el bucle principal...
+    // Código para la comunicación y operaciones después del handshake
 }
 
-void handshake() {
-    digitalWrite(SS, LOW); // Envía un 0
-    int respHandshake = 0;
-    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+void handshake(){
+  digitalWrite(SS, LOW); //envia un 0
+  int respHandshake=0;
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
   
-    int i = 0;
-    while (i < 3) {
-        respHandshake = SPI.transfer(5);
-        if (respHandshake == 0 && i == 2) {
-            Serial.println("HANDSHAKE CONFIRMADO");
-            handshakeConfirmation = true;
-            digitalWrite(SS, HIGH);
-            break;  
-        }
-        if (respHandshake == 0) {
-            i++;
-        }
-        if (respHandshake != 0) {
-            Serial.println("HANDSHAKE FALLIDO");
-            handshakeConfirmation = false;
-            break;  
-        }
-        delay(1000);
+  int i=0;
+  while (i<3){
+    respHandshake=SPI.transfer(5);
+    if(respHandshake==0&& i==2){ //debe recibir 0 3 veces para hacer el handshake
+      Serial.println("HANDSHAKE CONFIRMADO");
+      handshakeConfirmation=true;
+      digitalWrite(SS, HIGH); //al recibir el handshake establece el cs de la fpga en 1
+      break;  
     }
+    if(respHandshake==0){
+      i++;
+    }
+    if(respHandshake!=0){
+      Serial.println("HANDSHAKE FALLIDO");
+      handshakeConfirmation=false;
+      break;  
+    }
+    delay(1000);
+  }
 }
